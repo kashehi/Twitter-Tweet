b1f5404de538718f8a24ef74742e6ff18453bfd8
samaneh <kashehi2003@gmail.com>
2022-03-23 17:35:17 +0430

    Add HubClass And Code For Connect To twitter and recieve Tweets

--- Twittet-Tweet/Hub/TwitterHub.cs
+++ Twittet-Tweet/Hub/TwitterHub.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System.Threading.Tasks;
+using Tweetinvi;
+using System.Net;
+using Tweetinvi.Models;
+using System.Threading;
+using Tweetinvi.Streams;
 
-namespace Twittet_Tweet.Hub
+namespace Twitte_Tweet.Hubs
 {
-    public class TwitterHub : Hub
+    [HubName("hubTweet")]
+    public class HubTweetC : Hub
     {
-        public void Hello()
+        public async Task ConnectToTwitter()
         {
-            Clients.All.hello();
+            var userClient = new TwitterClient("054AHr7Ncjdytrj7YTRWtHCni", "XV7YDUIkxi7Z290TtHZxAu8DqxsUMFSmW6arzcnfIeBW2ldxPl", "1504021761372745731-bh9u5C5LiMzdtIbsq3YKOFvmZUwEY1", "xNP1p4ZapWbbLuxHvj38BzvW70gcC44daOPyqGtJzPiwb");
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            var sampleStream = userClient.Streams.CreateSampleStream();
+            sampleStream.TweetReceived += (sender, eventArgs) =>
+            {
+                Clients.All.broadcast(eventArgs.Tweet.FullText + ";" + eventArgs.Tweet.CreatedAt + ";" + eventArgs.Tweet.CreatedBy);
+            };
+
+            await sampleStream.StartAsync();
         }
     }
 }
